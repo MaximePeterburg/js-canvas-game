ebd1223e1a4d32573a92e85384d99a4ebab16b6d
MaximePeterburg <getmax4444@gmail.com>
2022-09-14 00:12:59 +0300

    Added starting cell generation, and neighbouring cells logic

--- index.js
+++ index.js
@@ -27,8 +27,31 @@ World.add(world, walls);
 
 //Maze Generation
 const grid = Array(cells).fill(null).map(() => Array(cells).fill(false));
-console.log(grid);
 
 const verticals = Array(cells).fill(null).map(() => Array(cells - 1).fill(false));
 const horizontals = Array(cells - 1).fill(null).map(() => Array(cells).fill(false));
-console.log(verticals, horizontals);
+
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+
+const stepThroughCell = (row, column) => {
+	// If i have visited the cell at [row, column], then return
+	if (grid[row][column] === true) {
+		return;
+	}
+	// Mark this cell as being visited
+	grid[row][column] = true;
+	// Assemble randomly-ordered list of neighbors
+	const neighbors = [ [ row - 1, column ], [ row, column + 1 ], [ row + 1, column ], [ row, column - 1 ] ];
+	// For each neighbor ...
+
+	// See if that neighbor is out of bounds
+
+	// If we have visited that neighbor, continue to next neighbour
+
+	// remove a wall from either horizontals or verticals
+
+	// visit that next cell
+};
+stepThroughCell(startRow, startColumn);
+console.log(grid);
