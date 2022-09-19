8fc61e598854f33736c770532cdd12660ff25a1b
MaximePeterburg <getmax4444@gmail.com>
2022-09-19 18:06:21 +0300

    Added checks for neighbour cells and vertical walls control

--- index.js
+++ index.js
@@ -27,6 +27,17 @@ World.add(world, walls);
 
 //Maze Generation
 const grid = Array(cells).fill(null).map(() => Array(cells).fill(false));
+const shuffle = (arr) => {
+	let counter = arr.length;
+	while (counter > 0) {
+		const index = Math.floor(Math.random() * counter);
+		counter--;
+		const temp = arr[counter];
+		arr[counter] = arr[index];
+		arr[index] = temp;
+	}
+	return arr;
+};
 
 const verticals = Array(cells).fill(null).map(() => Array(cells - 1).fill(false));
 const horizontals = Array(cells - 1).fill(null).map(() => Array(cells).fill(false));
@@ -42,16 +53,31 @@ const stepThroughCell = (row, column) => {
 	// Mark this cell as being visited
 	grid[row][column] = true;
 	// Assemble randomly-ordered list of neighbors
-	const neighbors = [ [ row - 1, column ], [ row, column + 1 ], [ row + 1, column ], [ row, column - 1 ] ];
+	const neighbors = shuffle([
+		// [ row - 1, column, 'up' ],
+		[ row, column + 1, 'right' ]
+		// [ row + 1, column, 'down' ],
+		// [ row, column - 1, 'left' ]
+	]);
+	console.log(neighbors);
 	// For each neighbor ...
-
-	// See if that neighbor is out of bounds
-
-	// If we have visited that neighbor, continue to next neighbour
-
-	// remove a wall from either horizontals or verticals
-
+	for (let neighbor of neighbors) {
+		const [ nextRow, nextColumn, direction ] = neighbor;
+		// See if that neighbor is out of bounds
+		if (nextRow < 0 || nextRow >= cells || nextColumn < 0 || nextColumn >= cells) {
+			continue;
+		}
+		// If we have visited that neighbor, continue to next neighbour
+		if (grid[nextRow][nextColumn]) {
+			continue;
+		}
+		// remove a wall from either horizontals or verticals
+		if (direction === 'left') {
+			verticals[row][column - 1] = true;
+		} else if (direction === 'right') {
+			verticals[row][column] = true;
+		}
+	}
 	// visit that next cell
 };
-stepThroughCell(startRow, startColumn);
-console.log(grid);
+stepThroughCell(1, 1);
